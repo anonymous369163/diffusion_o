4dfaf8a68753369096a3b0d08b36c0f059069aca
Albertyue3 <pyue515@gmail.com>
2025-06-05 01:59:19 +0000

    修改sh的位置

--- training_tsp_50.sh
+++ training_tsp_50.sh
@@ -2,14 +2,12 @@ export PYTHONPATH="$PWD:$PYTHONPATH"
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 
 # shellcheck disable=SC2155
-export WANDB_RUN_ID=$(python -c "import wandb; print(wandb.util.generate_id())")
-echo "WANDB_ID is $WANDB_RUN_ID"
+# export WANDB_RUN_ID=$(python -c "import wandb; print(wandb.util.generate_id())")
+# echo "WANDB_ID is $WANDB_RUN_ID"
 
 python -u difusco/train.py \
   --task "tsp" \
   --diffusion_type "categorical" \
-#   --do_train \
-#   --do_test \
   --learning_rate 0.0002 \
   --weight_decay 0.0001 \
   --lr_scheduler "cosine-decay" \
